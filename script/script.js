5389e7d86f4a79567fd9ba35db89b375e433087e
Leonardo Jorge <lpj.jorge@gmail.com>
2023-05-20 17:49:49 -0300

    Commit 7
    made some big progress in js, minimized some problems but i got a new mental one instead, the display none and block for the addTask is now function, but i'm having problems taking the information from the <form> and passing it to a new div in the content-list, seeking to resolve it as quick as possible for the major turn in this evening, this is a backup commit just so a dont lose that much progress if someting happens

--- script/script.js
+++ script/script.js
@@ -1,7 +1,27 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
+const toggleBtn = document.querySelector('#task');
+const toggleBtnOverlay = document.querySelector('#task-overlay');
+const divList = document.querySelector('.overlay');
+
+//overlay visible
+toggleBtn.addEventListener('click', () =>{
+  if(divList.style.display === 'none'){
+    divList.style.display = 'block';
+  }else {
+    divList.style.display = 'none';
   }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
+});
+
+toggleBtnOverlay.addEventListener('click', () =>{
+  if(divList.style.display === 'none'){
+    divList.style.display = 'block';
+  }else {
+    divList.style.display = 'none';
   }
+});
+
+//add task
+
+
+
+// create new task chart
+
